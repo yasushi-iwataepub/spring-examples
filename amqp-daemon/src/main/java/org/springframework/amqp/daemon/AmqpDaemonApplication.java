dc4468bf9ee2f69d0f329aed88189235cd6be3c9
Yasushi Iwata <yasushi@iwataepub.com>
2020-01-05 20:17:57 +0900

    changed to use CountDownLatch

--- amqp-daemon/src/main/java/org/springframework/amqp/daemon/AmqpDaemonApplication.java
+++ amqp-daemon/src/main/java/org/springframework/amqp/daemon/AmqpDaemonApplication.java
@@ -1,23 +1,32 @@
 package org.springframework.amqp.daemon;
 
+import java.util.concurrent.CountDownLatch;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
 public class AmqpDaemonApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AmqpDaemonApplication.class, args);
+	@Bean
+	public CountDownLatch closeLatch() {
+	    return new CountDownLatch(1);
 	}
 	
-	@Bean(name = "threadPoolTaskExecutor")
-	public ThreadPoolTaskExecutor taskExecutor() {
-		ThreadPoolTaskExecutor pool = new ThreadPoolTaskExecutor();
-		pool.setCorePoolSize(3);
-		pool.setMaxPoolSize(10);
-		pool.setWaitForTasksToCompleteOnShutdown(false);
-		return pool;
+	public static void main(String[] args) throws InterruptedException {
+		ApplicationContext ctx = SpringApplication.run(AmqpDaemonApplication.class, args);
+		
+		// https://stackoverflow.com/a/36065113
+	    final CountDownLatch closeLatch = ctx.getBean(CountDownLatch.class);
+	    Runtime.getRuntime().addShutdownHook(new Thread() {
+	        @Override
+	        public void run() {
+	            closeLatch.countDown();
+	        }
+	    });
+	    closeLatch.await();
 	}
 }
