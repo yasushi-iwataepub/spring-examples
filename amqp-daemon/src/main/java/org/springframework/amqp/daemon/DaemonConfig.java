24b2322d99ceed54ce072e578e84f1c124f90353
Yasushi Iwata <yasushi@iwataepub.com>
2020-01-05 17:57:28 +0900

    added rabbitListenerContainerFactory

--- amqp-daemon/src/main/java/org/springframework/amqp/daemon/DaemonConfig.java
+++ amqp-daemon/src/main/java/org/springframework/amqp/daemon/DaemonConfig.java
@@ -2,16 +2,28 @@ package org.springframework.amqp.daemon;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 
 @Configuration
+@EnableRabbit
 public class DaemonConfig {
 
     @Bean
     public Queue hello() {
         return new Queue("hello");
     }
+    
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory() {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(new CachingConnectionFactory());
+        factory.setConcurrentConsumers(3);
+        factory.setMaxConcurrentConsumers(10);
+        return factory;
+    }    
 
     @Bean
     public Receiver receiver() {
